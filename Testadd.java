dc3e774b4ead39ca63e97a5b44c68ecf0aaef661
Sudharma1997 <sudharmamokashi18@gmail.com>
2017-02-27 09:25:29 +0530

    Create Testadd.java

---
+++ Testadd.java
@@ -0,0 +1,15 @@
+public class Testadd
+{
+//Variable declaration and initialzation at the same time
+int a=10,b=20,c;
+float a1=10.0,b1=20.0,c1;
+  public static void main(String args[])//main function
+  {
+  //addition operation of two integers and storing result in c
+  c=a+b;
+  System.out.println("Addition of two integer is " +c );
+  //addition operation of two float numbers and storing result in c1
+  c1=a1+b1;
+  System.out.println("Addition of two float numbers is"+c1);
+  }
+}
